45bb309fb629139faf5cc80b84342a215f24ee31
채주영 <jy@chaejuyeong-ui-MacBookAir.local>
2026-04-13 14:58:28 +0900

    로그인 실패케이스 추가

--- test_tapas.py
+++ test_tapas.py
@@ -46,3 +46,23 @@ def test_tapas_login(driver):
         print("✅ 로그인 성공!")
     except TimeoutException:
         assert False, "로그인 실패 - 프로필 이미지를 찾을 수 없음"
+
+def test_tapas_login_fail(driver):
+    wait = WebDriverWait(driver, 15)
+    driver.get("https://tapas.io/")
+
+    login_button = driver.find_element(By.CSS_SELECTOR, "#__next > div > nav > div > div:nth-child(1) > div > div > div.flex.space-x-12 > a:nth-child(1)")
+    login_button.click()
+
+    email_input = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[type='email']")))
+    email_input.send_keys("dktjero@yopmail.com")
+
+    password_input = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input[type='password']")))
+    password_input.send_keys("notpassword")
+
+    submit_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[type='submit']")))
+    submit_button.click()
+
+    error_message = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#page-wrap > div > div > div.error-bg > div > p")))
+    assert "That email or password doesn't match" in error_message.text, f"테스트 실패! 메시지: {error_message.text}"
+    print("✅ 로그인 실패 메시지 정상 출력!")

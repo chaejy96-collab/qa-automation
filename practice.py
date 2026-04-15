ad7f553ff1b9e2a812c7db2eb592616df5b74406
채주영 <jy@chaejuyeong-ui-MacBookAir.local>
2026-04-15 14:22:53 +0900

    Git CI add

--- practice.py
+++ practice.py
@@ -14,9 +14,41 @@ def driver():
     driver.quit()
 
 def test_naver_search(driver):
-    driver.get("http://www.naver.com")
+    driver.get("https://www.naver.com")
 
     search_box = driver.find_element(By.CSS_SELECTOR, "#query")
     search_box.send_keys("손아섭 두산")
     search_box.send_keys(Keys.ENTER)
 
+    WebDriverWait(driver, 10).until(EC.title_contains("손아섭 두산"))
+
+    assert "손아섭 두산" in driver.title, f"테스트 실패 : {driver.title}"
+    print("✅ 테스트 통과!")
+
+def test_empty_search(driver):
+    driver.get("https://www.naver.com")
+
+    search_box = driver.find_element(By.CSS_SELECTOR, "#query")
+    search_box.send_keys("")
+    search_box.send_keys(Keys.ENTER)
+
+    WebDriverWait(driver, 10).until(EC.title_contains("네이버 검색"))
+
+    assert "네이버 검색" in driver.title, f"테스트 실패 : {driver.title}"
+    print("✅ 테스트 통과!")
+
+def test_blog_search(driver):
+    driver.get("https://www.naver.com")
+
+    search_box = driver.find_element(By.CSS_SELECTOR, "#query")
+    search_box.send_keys("손아섭")
+    search_box.send_keys(Keys.ENTER)
+
+    WebDriverWait(driver, 10).until(EC.title_contains("손아섭"))
+
+    blog_button = driver.find_element(By.CSS_SELECTOR, "#lnb > div.lnb_group > div > div.lnb_nav_area._nav_area_root > div > div.api_flicking_wrap._conveyer_root > div:nth-child(5) > a")
+    blog_button.click()
+
+    WebDriverWait(driver, 10).until(EC.title_contains("블로그"))
+    assert "블로그" in driver.title, f"테스트 실패 : {driver.title}"
+    print("✅ 테스트 통과!")

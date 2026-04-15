19334925513815096e741e43f9eee5f4b4de3051
채주영 <jy@chaejuyeong-ui-MacBookAir.local>
2026-04-15 15:14:17 +0900

    fix

--- conftest.py
+++ conftest.py
@@ -1,24 +1,17 @@
-import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import chromedriver_autoinstaller
-import os
+import pytest
 
 @pytest.fixture
 def driver():
     chromedriver_autoinstaller.install()
 
     options = Options()
-
-    if os.environ.get("HEADLESS") == "true":
-        options.add_argument("--headless=new")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")
-        options.add_argument("--remote-debugging-port=9222")  # ⭐ 중요
-        options.add_argument("--window-size=1920,1080")
-        options.add_argument("--single-process")  # ⭐ 추가
-        options.add_argument("--disable-extensions")  # ⭐ 추가
+    options.add_argument("--headless=new")  # 중요
+    options.add_argument("--no-sandbox")    # CI 필수
+    options.add_argument("--disable-dev-shm-usage")  # 메모리 이슈 방지
+    options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
     yield driver

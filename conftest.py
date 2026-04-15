790ef79b727297640449e9e5427b819bb83bdf0a
채주영 <jy@chaejuyeong-ui-MacBookAir.local>
2026-04-15 15:26:41 +0900

    fix

--- conftest.py
+++ conftest.py
@@ -1,14 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pytest
+import chromedriver_autoinstaller
 
 @pytest.fixture
 def driver():
+    chromedriver_autoinstaller.install()
+
     options = Options()
-    options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--headless=new")   # 핵심
+    options.add_argument("--no-sandbox")     # CI 필수
+    options.add_argument("--disable-dev-shm-usage")  # 메모리 이슈 방지
 
     driver = webdriver.Chrome(options=options)
     yield driver

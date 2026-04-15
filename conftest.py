27b6fe73d0f64b70e1a5cde8c83605a901e86545
채주영 <jy@chaejuyeong-ui-MacBookAir.local>
2026-04-15 15:02:42 +0900

    fix

--- conftest.py
+++ conftest.py
@@ -7,14 +7,16 @@ import os
 @pytest.fixture
 def driver():
     chromedriver_autoinstaller.install()
+
     options = Options()
 
     if os.environ.get("HEADLESS") == "true":
         options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--window-size=1920,1080")
         options.add_argument("--disable-gpu")
+        options.add_argument("--remote-debugging-port=9222")  # ⭐ 핵심
+        options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
     yield driver

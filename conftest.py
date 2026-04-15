302344a585c97c2539dbc449a797eafb64477cd6
채주영 <jy@chaejuyeong-ui-MacBookAir.local>
2026-04-15 15:47:47 +0900

    fix

--- conftest.py
+++ conftest.py
@@ -1,18 +1,14 @@
-import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+import pytest
+
 
 @pytest.fixture
 def driver():
     options = Options()
-    options.add_argument("--headless=new")
+    options.add_argument("--headless=new")  # ⭐ 핵심
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
     yield driver

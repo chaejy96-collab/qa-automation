ee826eacff897ca46b07e3f52aad5dadad1eccff
채주영 <jy@chaejuyeong-ui-MacBookAir.local>
2026-04-15 14:49:14 +0900

    update

--- conftest.py
+++ conftest.py
@@ -1,20 +1,20 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import chromedriver_autoinstaller
 import os
 
 
 @pytest.fixture
 def driver():
-    chromedriver_autoinstaller.install()
     options = Options()
+
     if os.environ.get("HEADLESS") == "true":
         options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--window-size=1920,1080")
         options.add_argument("--disable-gpu")
+
     driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
